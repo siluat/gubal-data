b03e3beca5a67242856bb4247586dc8b515ae689
Siluat <siluat@gmail.com>
2020-07-10 19:08:24 +0900

    ♻️ 빌드 관련 파일 구조 변경

---
+++ src/lib/buildItemSummaries.ts
@@ -0,0 +1,40 @@
+import { Row } from '../types/common';
+import { parseFile } from 'fast-csv';
+import { writeObjectToFile } from './file';
+
+type ItemSummary = [number, string, string, number, number, number];
+
+async function parseSheet(filepath: string): Promise<Row[]> {
+  const rows: Row[] = [];
+  return new Promise<Row[]>((resolve, reject) => {
+    parseFile(filepath)
+      .on('error', (error) => reject(error))
+      .on('data', (row: []) => Array.isArray(row) && rows.push(row))
+      .on('end', () => resolve(rows));
+  });
+}
+
+function createItemSummaries(itemRows: Row[]): ItemSummary[] {
+  // 검색 색인 생성에 필요한 데이터만 추출
+  return itemRows.map((row) => [
+    parseInt(row[0], 10), // key
+    row[10], // name
+    row[11], // icon
+    parseInt(row[12], 10), // item level
+    parseInt(row[13], 10), // rarity
+    parseInt(row[16], 10), // item ui category
+  ]);
+}
+
+function validateKey(itemSummary: ItemSummary): boolean {
+  const [key] = itemSummary;
+  return key !== 0 && (key < 40 || key > 1600);
+}
+
+export async function buildItemSummaries() {
+  const [, , , ...rows] = await parseSheet('./csv/Item.csv');
+  const itemSummaries: ItemSummary[] = createItemSummaries(rows).filter(
+    validateKey,
+  );
+  await writeObjectToFile('itemSummaries.json', itemSummaries);
+}
--- src/main.ts
+++ src/main.ts
@@ -1,50 +1,12 @@
-import { parseFile } from 'fast-csv';
-import {
-  createBuildDirectory,
-  cleanBuildDirectory,
-  writeObjectToFile,
-} from './lib/file';
-import { Row } from './types/common';
-
-type ItemSummary = [number, string, string, number, number, number];
-
-async function parseSheet(filepath: string): Promise<Row[]> {
-  const rows: Row[] = [];
-  return new Promise<Row[]>((resolve, reject) => {
-    parseFile(filepath)
-      .on('error', (error) => reject(error))
-      .on('data', (row: []) => Array.isArray(row) && rows.push(row))
-      .on('end', () => resolve(rows));
-  });
-}
-
-function createItemSummaries(itemRows: Row[]): ItemSummary[] {
-  // 검색 색인 생성에 필요한 데이터만 추출
-  return itemRows.map((row) => [
-    parseInt(row[0], 10), // key
-    row[10], // name
-    row[11], // icon
-    parseInt(row[12], 10), // item level
-    parseInt(row[13], 10), // rarity
-    parseInt(row[16], 10), // item ui category
-  ]);
-}
-
-function validateKey(itemSummary: ItemSummary): boolean {
-  const [key] = itemSummary;
-  return key !== 0 && (key < 40 || key > 1600);
-}
+import { createBuildDirectory, cleanBuildDirectory } from './lib/file';
+import { buildItemSummaries } from './lib/buildItemSummaries';
 
 async function main() {
   try {
     await cleanBuildDirectory();
     await createBuildDirectory();
 
-    const [, , , ...rows] = await parseSheet('./csv/Item.csv');
-    const itemSummaries: ItemSummary[] = createItemSummaries(rows).filter(
-      validateKey,
-    );
-    await writeObjectToFile('itemSummaries.json', itemSummaries);
+    await buildItemSummaries();
   } catch (error) {
     console.error(error);
   }
