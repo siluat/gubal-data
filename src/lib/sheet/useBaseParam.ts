9938ec5cf89dc73e75891077e284077a4e6c7852
siluat <siluat@gmail.com>
2020-08-06 17:56:29 +0900

    🐛 BaseParam 데이터 파싱 오류 수정

--- src/lib/sheet/useBaseParam.ts
+++ src/lib/sheet/useBaseParam.ts
@@ -10,8 +10,8 @@ type BaseParam = {
 function createBaseParam(rows: Row[]): BaseParam[] {
   return rows.map((row) => ({
     id: parseInt(row[0], 10),
-    name: row[1],
-    description: row[2],
+    name: row[2],
+    description: row[3],
   }));
 }
 
